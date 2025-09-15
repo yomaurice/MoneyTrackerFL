a8282411301d42edad29577f0fcab08e8419d87a
yomaurice <74863761+yomaurice@users.noreply.github.com>
2025-09-15 19:55:39 +0300

    fix cors

--- backend/App.py
+++ backend/App.py
@@ -20,7 +20,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 # CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
-CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "http://localhost:3000"}}, methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+# CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "http://localhost:3000"}}, methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 
 with app.app_context():
     db.create_all()
