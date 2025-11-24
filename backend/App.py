ba656e80bd7e470e2894d86d5ece015d7de2d76c
yomaurice <74863761+yomaurice@users.noreply.github.com>
2025-11-24 20:50:11 +0200

    rollback signup with new db

--- backend/App.py
+++ backend/App.py
@@ -23,7 +23,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "connect_args": {
-        "sslmode": "disable"
+        "sslmode": "require"
     }
 }
 
