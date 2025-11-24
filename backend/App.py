36e4e01eaf03e53a804e84752554b9ffca0f877a
yomaurice <74863761+yomaurice@users.noreply.github.com>
2025-11-24 16:10:04 +0200

    fix env

--- backend/App.py
+++ backend/App.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 # app.config['SECRET_KEY'] = 'your-secret-key'
 app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-key")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     "connect_args": {
