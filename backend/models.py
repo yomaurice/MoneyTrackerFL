9269570157a08755c1742466ad5da42eb460fb7e
yomaurice <74863761+yomaurice@users.noreply.github.com>
2025-12-11 15:19:21 +0200

    add_modules

--- backend/models.py
+++ backend/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)   # NEW
     password_hash = db.Column(db.String(256), nullable=False)
 
     def set_password(self, password):
